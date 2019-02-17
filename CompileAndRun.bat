20031c63a8f2a55f9abee7237f0af3c72000d35b
Pascualex <alejandropascualpozo@gmail.com>
2019-02-17 19:34:45 +0100

    Añadido compilador automático

---
+++ CompileAndRun.bat
@@ -0,0 +1,2 @@
+javac ./src/java/*.java ./src/java/modules/*.java ./src/java/objects/*.java
+java src.java.Main
--- src/java/Main.java
+++ src/java/Main.java
@@ -5,6 +5,6 @@ import src.java.App;
 public class Main {
 
     public static void main(String[] args) {
-        App app = new App();        
+        App app = new App();
     }
 }
