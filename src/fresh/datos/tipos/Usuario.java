97b4499670033cd1f28b3bf04ee930e65c6132e7
AnaRoa <ana.roaglz@gmail.com>
2019-03-06 22:01:59 +0100

    Implementacion de funciones get de usuario

--- src/fresh/datos/tipos/Usuario.java
+++ src/fresh/datos/tipos/Usuario.java
@@ -1,5 +1,66 @@
 package fresh.datos.tipos;
 
+/**
+ * 
+ * 	Implementación de la clase Usuario
+ * 
+ * 	@author 
+ */
+
 public class Usuario {
-    
+	
+	private boolean esPremium;
+	private String nombre;
+	private String contrasena;
+	private Date fechaNacimiento;
+	private int reproduccionesMensuales;
+	
+	private List<Notificacion> notificaciones;
+	private List<ListaReproduccion> listasReproduccion;
+	private List<Cancion> canciones;
+	private List<Album> albumes;
+	
+	public Usuario(boolean esPremium, String nombre, String contrasena, Date fechaNacimiento) {
+		this.esPremium = esPremium;
+		this.nombre = nombre;
+		this.contrasena = contrasena;
+		this.fechaNacimiento = fechaNacimiento;
+		
+	}
+	
+	public boolean getEsPremium(){
+		return esPremium;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public String getContrasena(){
+		return contrasena;
+	}
+	
+	public Date getFechaNacimiento(){
+		return fechaNacimiento;
+	}
+	
+	public int getReproduccionesMensuales(){
+		return reproduccionesMensuales;
+	}
+	
+	public List<Notificacion> getNotificaciones(){
+		return notificaciones;
+	}
+	
+	public List<ListaReproduccion> getListasReproduccion(){
+		return listasReproduccion;
+	}
+	
+	public List<Cancion> getCanciones(){
+		return canciones;
+	}
+	
+    public List<Album> getAlbumes(){
+    	return albumes;
+    }
 }
