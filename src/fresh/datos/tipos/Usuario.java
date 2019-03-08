fd35ad8c4d42301f732c344beae8904cc9177e6f
AnaRoa <ana.roaglz@gmail.com>
2019-03-08 10:05:40 +0100

    añadir funcion a usuario

--- src/fresh/datos/tipos/Usuario.java
+++ src/fresh/datos/tipos/Usuario.java
@@ -1,4 +1,7 @@
 package fresh.datos.tipos;
+import java.util.Calendar;
+import java.time.Period;
+import java.time.LocalDate;
 
 /**
  * 
@@ -11,8 +14,9 @@ public class Usuario {
 	
 	private boolean esPremium;
 	private String nombre;
+	private String nombreAutor;
 	private String contrasena;
-	private Date fechaNacimiento;
+	private Calendar fechaNacimiento;
 	private int reproduccionesMensuales;
 	
 	private List<Notificacion> notificaciones;
@@ -20,9 +24,20 @@ public class Usuario {
 	private List<Cancion> canciones;
 	private List<Album> albumes;
 	
-	public Usuario(boolean esPremium, String nombre, String contrasena, Date fechaNacimiento) {
+	/**
+	 * Constructor de la clase Usuario
+	 * 
+	 * @param esPremium, indica si el usuario es Premium
+	 * @param nombre, nombre del Usuario
+	 * @param nombreAutor, nombre de autor del Usuario
+	 * @param contrasena, contraseña con la que se registra el Usuario
+	 * @param fechaNacimiento, fecha de nacimento del usuario
+	 */
+	
+	public Usuario(boolean esPremium, String nombre, String nombreAutor, String contrasena, Date fechaNacimiento) {
 		this.esPremium = esPremium;
 		this.nombre = nombre;
+		this.nombreAutor = nombreAutor;
 		this.contrasena = contrasena;
 		this.fechaNacimiento = fechaNacimiento;
 		
@@ -36,11 +51,16 @@ public class Usuario {
 		return nombre;
 	}
 	
+	public String getNombreAutor(){
+		return nombreAutor;
+	}
+	
+	
 	public String getContrasena(){
 		return contrasena;
 	}
 	
-	public Date getFechaNacimiento(){
+	public Calendar getFechaNacimiento(){
 		return fechaNacimiento;
 	}
 	
@@ -62,5 +82,24 @@ public class Usuario {
 	
     public List<Album> getAlbumes(){
     	return albumes;
+    }
+    
+    /**
+     * Funcion que calcula si un Usuario es mayor de edad para reproducir contenido explicito
+     * 
+     * @return true si puede reproducir contenido explicito o false si no
+     */
+    
+    public boolean puedeContenidoExplicito() {
+    	
+    	LocalDate ahora = LocalDate.now();
+    	Period periodo = Period.between(fechaNacimiento, ahora);
+    	
+    	if (periodo.getYears() > 18) {
+    		return true;;
+    	}
+    	
+    	return false;
+    	
     }
 }
