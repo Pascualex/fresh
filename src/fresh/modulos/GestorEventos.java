c3faef4eb04b72bef6b9293eb9fddb95148c69da
Víctor Yrazusta <victoryrazustaibarra@gmail.com>
2019-03-08 11:09:30 +0100

    Continuación del gestor de eventos

--- src/fresh/modulos/GestorEventos.java
+++ src/fresh/modulos/GestorEventos.java
@@ -1,6 +1,5 @@
 package fresh.modulos;
 
-import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -38,7 +37,7 @@ public class GestorEventos{
      * @param baseDeDatos Base de datos relacionada con el gestor.
      */
     public GestorEventos(String ruta, BaseDeDatos baseDeDatos) {
-
+        //Tenemos que especificar como vamos a guardar la información en disco
     }
 
     /**
@@ -57,30 +56,48 @@ public class GestorEventos{
      * @param ruta Ruta en la que guardar la información del gestor.
      */
     public boolean GuargarInformacion(String ruta) {
+        //Tenemos que especificar como vamos a guardar la información en disco
+        return false;
+    }
 
+    public boolean ProgramarEliminacionCancion(Cancion cancion) {
+        return cancionesAEliminar.add(new ParCancionFecha(cancion));
     }
     
     /**
      * Inicia el proceso de comprobar las canciones a eliminar y degradar a los
      * usuarios a usuarios normales quitándoles el premium a inicio de mes.
      */
-    public void Iniciar() {
+    public void Iniciar() throws InterruptedException{
         while (true) {
             //Calculo la fecha actual
             GregorianCalendar fecha_actual = new GregorianCalendar();
             //Si no ha pasado un día aún espero a que sea el día que viene
-            if (fecha_actual.getTime()-fecha.getTime() < msDia) {
-                TimeUnit.SECONDS.sleep((fecha_actual.getTime()-msDia)/1000);
+            if (fecha_actual.getTimeInMillis()-ultimoDiaComprobado.getTimeInMillis() < msDia) {
+                TimeUnit.SECONDS.sleep(2000+(fecha_actual.getTimeInMillis()-msDia)/1000);
             }
             
             //He de realizar las operaciones
 
             //-----------
             //Operaciones
+            fecha_actual = new GregorianCalendar();
+            //Degradar a todos los usuarios (en caso de que sea necesario)
+            if (fecha_actual.get(fecha_actual.MONTH) != ultimoDiaComprobado.get(ultimoDiaComprobado.MONTH)) {
+                baseDeDatos.eliminarPremiumUsuarios();
+            }
+
+            //Eliminar todas las canciones necesarias
+            for (ParCancionFecha par : cancionesAEliminar) {
+                if (fecha_actual.getTimeInMillis()-par.fecha.getTimeInMillis() > 3*msDia) {
+                    //Eliminar cancion
+                }
+            }
+
             //-----------
 
             //Reestablezco el tiempo
-            fecha = new GregorianCalendar();
+            ultimoDiaComprobado = new GregorianCalendar();
         }
     }
 
