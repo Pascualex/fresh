5c535dedf21bbec8325baa9826527cf02e7fa382
Víctor Yrazusta <victoryrazustaibarra@gmail.com>
2019-03-19 10:36:27 +0100

    Comentadas las notficaciones

--- src/fresh/datos/tipos/Notificacion.java
+++ src/fresh/datos/tipos/Notificacion.java
@@ -1,12 +1,24 @@
 package fresh.datos.tipos;
 
+/**
+ * Permite trabajar con notificaciones en el sistema.
+ * @author Víctor Yrazusta (victor.yrazusta@estudiante.uam.es)
+ */
 public class Notificacion {
     private TipoNotificacion tipoNotificacion;
 
+    /**
+     * Instancia una notificación dado su tipo.
+     * @param tipoNotificacion Tipo de la notificación.
+     */
     public Notificacion(TipoNotificacion tipoNotificacion) {
         this.tipoNotificacion = tipoNotificacion;
     }
 
+    /**
+     * Devuelve el tipo de la notificación.
+     * @return Tipo de la notificación.
+     */
     public TipoNotificacion getTipoNotificacion() {
         return tipoNotificacion;
     }
--- src/fresh/datos/tipos/NotificacionCancionAutor.java
+++ src/fresh/datos/tipos/NotificacionCancionAutor.java
@@ -1,14 +1,29 @@
 package fresh.datos.tipos;
 
+/**
+ * Permite trabajar con notificaciones relacionadas con una canción y un autor
+ * en el sistema.
+ * @author Víctor Yrazusta (victor.yrazusta@estudiante.uam.es)
+ */
 public class NotificacionCancionAutor extends NotificacionCancion {
     private Usuario autor;
 
+    /**
+     * Instancia una notificación dado su tipo, canción y autor relacionados.
+     * @param tipoNotificacion Tipo de la notificación.
+     * @param cancion Canción relacionada con la notificación.
+     * @param autor Autor relacionado con la notificación.
+     */
     public NotificacionCancionAutor(TipoNotificacion tipoNotificacion, 
                                     Cancion cancion, Usuario autor) {
         super(tipoNotificacion, cancion);
         this.autor = autor;
     }
 
+    /**
+     * Devuelve el autor relacionado con la notificación.
+     * @return Autor relacionado con la notifiación.
+     */
     public Usuario getAutor() {
         return autor;
     }
