dc0ee16b2eb893af867c62e5575c239e7d13d6e8
Victor Yrazusta <victoryrazustaibarra@gmail.com>
2019-03-27 20:13:49 +0100

    ModuloMP3 v1.0

--- src/fresh/modulos/ModuloMP3.java
+++ src/fresh/modulos/ModuloMP3.java
@@ -1,5 +1,63 @@
 package fresh.modulos;
 
-public class ModuloMP3 {
-    
+import fresh.datos.tipos.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.embed.swing.JFXPanel;
+import javafx.application.Platform;
+
+public class ModuloMP3 implements Runnable {
+    private MediaPlayer mediaPlayer = null;
+    private List<Cancion> colaReproduccion = new ArrayList<>();
+    private final JFXPanel fxPanel = new JFXPanel();
+    private ModuloMP3 sig = null;
+
+    public void anadirAColaReproduccion(List<Cancion> canciones) {
+        if (sig != null) {
+            sig.anadirAColaReproduccion(canciones);
+        } else {
+            colaReproduccion.addAll(canciones);
+        }
+    }
+
+    public void run() {
+        while(colaReproduccion.isEmpty());
+
+        Cancion cancion = colaReproduccion.get(0);
+        colaReproduccion.remove(0);
+
+        String nombre = cancion.getId() + ".mp3";
+        Media media = new Media(new File(nombre).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
+        //Operaciones con la canción
+
+        mediaPlayer.play();
+        mediaPlayer.setOnEndOfMedia(this);
+    }
+
+    public synchronized void pause() {
+        if (sig != null) sig.pause();
+        if (mediaPlayer != null) mediaPlayer.pause();
+    }
+
+    public synchronized void play() {
+        if (sig != null) sig.play();
+        if (mediaPlayer != null) mediaPlayer.play();
+    }
+
+    public synchronized void siguente() {
+        sig = new ModuloMP3();
+        mediaPlayer.stop();
+        sig.anadirAColaReproduccion(colaReproduccion);
+        sig.run();
+    }
+
+    public synchronized void exit() {
+        if (sig != null) sig.exit();
+        Platform.exit();
+    }
 }
