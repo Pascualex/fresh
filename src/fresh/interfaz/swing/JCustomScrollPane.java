f82b64f2308103a3b738edba2e88b2f5465b967b
Víctor Yrazusta <victoryrazustaibarra@gmail.com>
2019-04-27 18:19:17 +0200

    Scroll no funcionando

---
+++ src/fresh/interfaz/swing/JCustomScrollPane.java
@@ -0,0 +1,14 @@
+package fresh.interfaz.swing;
+
+import java.awt.Component;
+
+import javax.swing.JScrollPane;
+
+public class JCustomScrollPane extends JScrollPane {
+    private static final long serialVersionUID = 0;
+    
+
+    public JCustomScrollPane(Component c) {
+        super(c);
+    }
+}
--- src/fresh/interfaz/vistas/VistaPlaylists.java
+++ src/fresh/interfaz/vistas/VistaPlaylists.java
@@ -2,14 +2,37 @@ package fresh.interfaz.vistas;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import fresh.interfaz.*;
+import fresh.interfaz.swing.JCustomScrollPane;
 
 public class VistaPlaylists extends JPanel {
     private static final long serialVersionUID = 0;
+
+    public JPanel panelScroll;
+    public JCustomScrollPane scroll;
 
     public VistaPlaylists() {
         setBounds(260, 120, Estilo.anchura-260, 680-120);
         setOpaque(false);
         setLayout(null);
+        setPreferredSize(new Dimension(Estilo.anchura, Estilo.altura));
+
+        panelScroll = new JPanel();        
+        panelScroll.setBounds(0, 0, Estilo.anchura-260, 680-120);
+        panelScroll.setPreferredSize(new Dimension(Estilo.anchura, Estilo.altura));    
+        panelScroll.setOpaque(true);
+        panelScroll.setBackground(new Color(255, 0, 0));
+        panelScroll.setLayout(null);
+        panelScroll.setVisible(true);
+        add(panelScroll);
+
+        scroll = new JCustomScrollPane(panelScroll);
+        scroll.setVerticalScrollBarPolicy(JCustomScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.setVisible(true);
+        add(scroll);      
+        
+        setVisible(true);
     }    
 }
